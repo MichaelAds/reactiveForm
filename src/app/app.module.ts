e64ef31c6b33db06b2acdb98eedee0b8a87f8403
Michaelads <maike_maicon09@hotmail.com>
2019-10-17 22:20:16 -0300

    add project reactive form

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
